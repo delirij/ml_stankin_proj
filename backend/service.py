e41c25b7f79f89e558da4caa40847bcb2bc9069b
sadxyMath <matvejzralko@gmail.com>
2025-11-17 01:31:58 +0300

    сделал фронт и связал с бэком. все вместе запускается через докер

--- backend/service.py
+++ backend/service.py
@@ -319,6 +319,13 @@ def file_bytes_to_scenes(file_bytes: bytes, filename: str) -> List[str]:
 
 
 # --------- Основной эндпоинт ---------
+@app.get("/")
+def read_root():
+    return {"message": "Hello World"}
+    
+@app.get("/health")
+def health():
+    return {"status": "ok"}
 
 
 @app.post("/analyze_file")
